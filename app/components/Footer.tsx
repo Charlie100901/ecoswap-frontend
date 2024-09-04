3973111753aad04cd7d430e2817f33a290b05118
carlos <carlosdelarosa1009@gmail.com>
2024-09-04 10:53:11 -0500

    quienes somos component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Footer() {
+    return(
+        <footer className="max-h-60 bg-lime-700">
+            <h2>Footer</h2>
+        </footer>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,16 @@
 "use client"
 import Image from 'next/image';
 import Header from './components/Header';
-import { useEffect, useState } from 'react';
+import QuienesSomos from './components/QuienesSomos';
+import Footer from './components/Footer';
 
 export default function Home() {
-  const fullText = "Intercambia lo que Tienes, Obtén lo que Necesitas";
-  const [displayedText, setDisplayedText] = useState("");
-
-  useEffect(() => {
-    let index = 0;
-    const interval = setInterval(() => {
-      if (index < fullText.length) {
-        setDisplayedText((prev) => prev + fullText.charAt(index)); // Usar charAt para evitar undefined
-        index++;
-      } else {
-        clearInterval(interval);
-      }
-    }, 100); // Ajusta la velocidad de la animación aquí
-
-    return () => clearInterval(interval);
-  }, []); // Dependencia de fullText
 
   return (
     <div>
       <Header />
       <main className="flex min-h-screen flex-col items-center justify-between">
         <div className="relative w-full h-[700px]">
-          {/* Imagen de fondo */}
           <Image
             src="/img/home.jpg"
             alt="Home Image"
@@ -35,11 +19,15 @@ export default function Home() {
             className="absolute inset-0 w-full h-full object-cover filter brightness-[40%]"
           />
 
-          <p className="absolute inset-0 flex items-center justify-center text-white text-4xl font-bold z-50">
-            {displayedText}
+          <p className="absolute inset-0 flex items-center justify-center text-white text-4xl font-bold z-40">
+            Intercambia lo que Tienes, Obtén lo que Necesitas
           </p>
+          <button className="absolute top-[60%] transform -translate-x-1/2 left-1/2 text-white bg-lime-700 hover:bg-green-700 font-semibold py-2 px-4 rounded mt-4 z-40">Empieza a Intercambiar</button>
         </div>
+        <QuienesSomos/>
+        
       </main>
+      <Footer/>
     </div>
   );
 }
