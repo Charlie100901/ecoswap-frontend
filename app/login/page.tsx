c5b96ae62cdb5a993954e16173b80e0a01fcb4ec
carlos <carlosdelarosa1009@gmail.com>
2024-09-04 01:35:58 -0500

    first commit

---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header"
+
+
+export default function Page() {
+    return (
+      <div>
+        <Header></Header>
+        <h1>Hello, Login page!</h1>
+      </div>
+    );
+  }
