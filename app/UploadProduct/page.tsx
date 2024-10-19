f63c6aaca5c5767a081d69be7a586992fde66b7f
carlos <carlosdelarosa1009@gmail.com>
2024-10-19 00:27:04 -0500

    subir producto y mostrar nombre de usuario en header

--- app/UploadProduct/page.tsx
+++ app/UploadProduct/page.tsx
@@ -1,56 +1,187 @@
+"use client";
+
+import { useState, ChangeEvent, FormEvent } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
+interface FormData {
+    title: string;
+    category: string;
+    conditionProduct: string;
+    file: File | null; 
+    description: string;
+}
+
 export default function Page() {
+    const [formData, setFormData] = useState<FormData>({
+        title: '',
+        category: '',
+        conditionProduct: '',
+        file: null,
+        description: ''
+    });
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
+        const { name, value, files } = e.target as HTMLInputElement; 
+
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: files ? files[0] : value 
+        }));
+    };
+
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const formDataToSend = new FormData();
+        for (const key in formData) {
+            formDataToSend.append(key, formData[key as keyof FormData] as any); 
+        }
+
+        try {
+            const token = localStorage.getItem('token');
+
+            const requestOptions: RequestInit = {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                },
+                body: formDataToSend
+            };
+
+            const response = await fetch('http://localhost:8080/api/v1/product/create', requestOptions);
+
+            if (response.ok) {
+                const responseData = await response.text(); 
+                if (responseData) {
+                    const jsonResponse = JSON.parse(responseData); 
+                    setIsModalOpen(true); // Mostrar el modal
+                    // Resetear los campos del formulario
+                    setFormData({
+                        title: '',
+                        category: '',
+                        conditionProduct: '',
+                        file: null,
+                        description: ''
+                    });
+                    console.log(jsonResponse);
+                } else {
+                    console.error('La respuesta está vacía');
+                }
+            } else {
+                console.error('Error al subir el producto:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Error al enviar la solicitud:', error);
+        }
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false); // Cerrar el modal
+    };
+
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
-
-            <main className="flex-grow">
+            <main className="flex-grow mb-9 mx-32 p-4">
                 <div>
                     <h1 className="text-center mt-14 mb-12 text-2xl font-bold">CARGAR PRODUCTO</h1>
                 </div>
                 <div className="flex justify-between mx-12">
-                    <form className="w-full">
+                    <form className="w-full" onSubmit={handleSubmit}>
                         <div className="flex justify-between">
                             <div className="w-1/2 mr-4">
                                 <label className="block font-medium">Titulo</label>
-                                <input type="text" className="block w-3/4 pt-2 border border-gray-300 rounded-md " />
+                                <input
+                                    type="text"
+                                    className="block w-3/4 pt-2 border border-gray-300 rounded-md"
+                                    name="title"
+                                    value={formData.title} // Añadido para controlar el valor del input
+                                    onChange={handleChange} 
+                                />
                             </div>
                             <div className="w-1/2 ml-4">
                                 <label className="block font-medium">Estado del producto</label>
-                                <select className="form-select block w-3/4 pt-2 border border-gray-300 rounded-md" >
-                                    <option>Selecciona una opción</option>
-                                    <option value="Nuevo">Nuevo</option>
-                                    <option value="Casi Nuevo">Casi Nuevo</option>
-                                    <option value="Usado">Usado</option>
+                                <select
+                                    className="block w-3/4 pt-2 border border-gray-300 rounded-md"
+                                    name="conditionProduct"
+                                    value={formData.conditionProduct} // Añadido para controlar el valor del select
+                                    onChange={handleChange} 
+                                >
+                                    <option value="">Selecciona una opción</option>
+                                    <option value="nuevo">Nuevo</option>
+                                    <option value="casi nuevo">Casi Nuevo</option>
+                                    <option value="usado">Usado</option>
                                 </select>
                             </div>
                         </div>
-                        <div className="w-1/2 mr-4 mt-5 ">
+                        <div className="w-1/2 mr-4 mt-5">
                             <label className="block font-medium">Categoria</label>
-                            <select className="form-select block w-3/4 pt-2 border border-gray-300 rounded-md">
-                                <option>Selecciona una opción</option>
-                                <option value="tecnologia">Tecnología</option>
-                                <option value="arte">Arte</option>
-                                <option value="deporte">Deporte</option>
-                                <option value="ropa">Ropa</option>
-                                <option value="juguete">Juguetes</option>
-                                <option value="hogar">Hogar</option>
+                            <select
+                                className="block w-3/4 pt-2 border border-gray-300 rounded-md"
+                                name="category"
+                                value={formData.category} // Añadido para controlar el valor del select
+                                onChange={handleChange}
+                            >
+                                <option value="">Selecciona una opción</option>
+                                <option value="Electrónica y Tecnología">Electrónica y Tecnología</option>
+                                <option value="Hogar y Muebles">Hogar y Muebles</option>
+                                <option value="Juguetes y Accesorios">Juguetes y Accesorios</option>
+                                <option value="Libros y Entretenimiento">Libros y Entretenimiento</option>
+                                <option value="Deportes y Aire Libre">Deportes y Aire Libre</option>
+                                <option value="Ropa y Accesorios">Ropa y Accesorios</option>
+                                <option value="Mascotas">Mascotas</option>
+                                <option value="Oficina y Papelería">Oficina y Papelería</option>
+                                <option value="Salud y Belleza">Salud y Belleza</option>
+                                <option value="Joyería y Relojes">Joyería y Relojes</option>
                             </select>
                         </div>
                         <div className="w-1/2 mr-4 mt-5">
                             <label className="block font-medium">Descripción del Producto</label>
-                            <textarea className="block w-3/4 pt-2 border border-gray-300 rounded-md h-32" />
+                            <textarea
+                                className="block w-3/4 pt-2 border border-gray-300 rounded-md h-32"
+                                name="description"
+                                value={formData.description} // Añadido para controlar el valor del textarea
+                                onChange={handleChange} 
+                            />
                         </div>
                         <div className="w-1/2 mr-4 mt-5">
                             <label className="block font-medium">Imagen del Producto</label>
-                            <input className="block w-3/4 pt-2 border border-gray-300 rounded-md mb-12" type="file" accept="image/png, image/jpeg" />
+                            <input
+                                className="block w-3/4 pt-2 border border-gray-300 rounded-md mb-12"
+                                type="file"
+                                name="file"
+                                accept="image/png, image/jpeg"
+                                onChange={handleChange} 
+                            />
                         </div>
+
+                        <button
+                            type="submit"
+                            className="bg-blue-500 text-white py-2 px-4 rounded"
+                        >
+                            Subir producto
+                        </button>
                     </form>
                 </div>
-            </main>
 
+                {/* Modal */}
+                {isModalOpen && (
+                    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+                        <div className="bg-white p-6 rounded-lg shadow-lg">
+                            <h2 className="text-lg font-bold mb-4">Producto creado exitosamente</h2>
+                            <button
+                                className="bg-blue-500 text-white py-2 px-4 rounded"
+                                onClick={closeModal}
+                            >
+                                Cerrar
+                            </button>
+                        </div>
+                    </div>
+                )}
+            </main>
             <Footer />
         </div>
     );
